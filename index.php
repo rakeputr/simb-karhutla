16f3d2c73128ed983e80339ff8ccd7ebb6561cdd
rakeputr <rakeputri27@gmail.com>
2024-11-13 19:49:48 +0700

    change template index

--- index.php
+++ index.php
@@ -1,23 +1,4 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <title>Pantau Api</title>
-        <!-- CSS FILES -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;700;900&display=swap" rel="stylesheet">
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/bootstrap-icons.css" rel="stylesheet">
-        <link rel="stylesheet" href="css/magnific-popup.css">
-        <link href="css/aos.css" rel="stylesheet">
-        <link href="css/templatemo-nomad-force.css" rel="stylesheet">
-    </head>
-    
-    <body>
+<?php include (__DIR__ . '/src/Templates/header.php'); ?>
         <main>
 
             <section class="hero" id="hero">
@@ -42,41 +23,7 @@
                 <div class="overlay"></div>
             </section>
 
-            <nav class="navbar navbar-expand-lg bg-dark-subtle shadow-lg">
-                <div class="container">
-                    <a class="navbar-brand" href="index.html">
-                        <strong>Pantau Api</strong>
-                    </a>
-
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav mx-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#hero">Home</a>
-                            </li>
-
-                            <li class="nav-item">
-                                <a class="nav-link" href="#about">Our Studio</a>
-                            </li>
-
-                            <li class="nav-item">
-                                <a class="nav-link" href="#portfolio">Portfolio</a>
-                            </li>
-
-                            <li class="nav-item">
-                                <a class="nav-link" href="#news">Berita</a>
-                            </li>
-
-                            <li class="nav-item">
-                                <a class="nav-link" href="#contact">Contact Us</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+            <?php include (__DIR__ . '/src/Templates/navbar.php') ?>
 
             <section class="section-padding pb-0" id="about">
                 <div class="container mb-5 pb-lg-5">
